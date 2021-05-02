a6efaaf36165dbe25a24d687c37a70cb561a8730
Nick <nick@mackenzie.tech>
2021-05-01 21:45:50 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,108 @@ export default function Home() {
         <meta name="description" content="MacKenzie Photography" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Hero isColor="info" isSize="medium">
+        <HeroHeader>
+          <Nav>
+            <NavLeft>
+              <NavItem isBrand>Bloomer</NavItem>
+            </NavLeft>
+            <NavCenter>
+              <NavItem>
+                <Icon className="fa fa-github" />
+              </NavItem>
+              <NavItem>
+                <Icon className="fa fa-twitter" />
+              </NavItem>
+            </NavCenter>
+            <NavRight isMenu>
+              <NavItem>Home</NavItem>
+              <NavItem>Documentation</NavItem>
+            </NavRight>
+          </Nav>
+        </HeroHeader>
+
+        <HeroBody>
+          <Container hasTextAlign="centered">
+            <Title>Title</Title>
+          </Container>
+        </HeroBody>
+
+        <HeroFooter>
+          <Tabs isBoxed isFullWidth>
+            <Container>
+              <TabList>
+                <Tab isActive>
+                  <TabLink>Overview</TabLink>
+                </Tab>
+                <Tab>
+                  <TabLink>Grid</TabLink>
+                </Tab>
+                <Tab>
+                  <TabLink>Element</TabLink>
+                </Tab>
+                <Tab>
+                  <TabLink>Components</TabLink>
+                </Tab>
+                <Tab>
+                  <TabLink>Layout</TabLink>
+                </Tab>
+              </TabList>
+            </Container>
+          </Tabs>
+        </HeroFooter>
+      </Hero>
+      <Hero isColor="info" isSize="medium">
+        <HeroHeader>
+          <Nav>
+            <NavLeft>
+              <NavItem isBrand>Bloomer</NavItem>
+            </NavLeft>
+            <NavCenter>
+              <NavItem>
+                <Icon className="fa fa-github" />
+              </NavItem>
+              <NavItem>
+                <Icon className="fa fa-twitter" />
+              </NavItem>
+            </NavCenter>
+            <NavRight isMenu>
+              <NavItem>Home</NavItem>
+              <NavItem>Documentation</NavItem>
+            </NavRight>
+          </Nav>
+        </HeroHeader>
+
+        <HeroBody>
+          <Container hasTextAlign="centered">
+            <Title>Title</Title>
+          </Container>
+        </HeroBody>
+
+        <HeroFooter>
+          <Tabs isBoxed isFullWidth>
+            <Container>
+              <TabList>
+                <Tab isActive>
+                  <TabLink>Overview</TabLink>
+                </Tab>
+                <Tab>
+                  <TabLink>Grid</TabLink>
+                </Tab>
+                <Tab>
+                  <TabLink>Element</TabLink>
+                </Tab>
+                <Tab>
+                  <TabLink>Components</TabLink>
+                </Tab>
+                <Tab>
+                  <TabLink>Layout</TabLink>
+                </Tab>
+              </TabList>
+            </Container>
+          </Tabs>
+        </HeroFooter>
+      </Hero>
     </>
   );
 }
